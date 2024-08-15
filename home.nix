de5495a3681b099b91e2f7be41ad6572a0794bbe
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-15 17:46:36 -0500

    update config

--- home.nix
+++ home.nix
@@ -204,8 +204,8 @@
     shellAliases = {
       ll = "ls -l";
       update = "sudo nixos-rebuild switch";
-      gnrs = "git add . && git commit -m "update config" && git push && sudo nixos-rebuild switch --flake "/home/shlok/nixos-config" | lolcat -f";
-      nrs = "sudo nixos-rebuild switch --flake "/home/shlok/nixos-config" | lolcat -f";
+      gnrs = "git add . && git commit -m \"update config\" && git push && sudo nixos-rebuild switch --flake "/home/shlok/nixos-config" | lolcat -f";
+      nrs = "sudo nixos-rebuild switch --flake \"/home/shlok/nixos-config\" | lolcat -f";
     };
     history = {
       size = 10000;
