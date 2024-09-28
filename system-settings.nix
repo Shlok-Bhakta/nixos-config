727b5f7973733514cd10006e7967ef50772952b0
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-09-27 20:26:56 -0500

    update kernel version

--- system-settings.nix
+++ system-settings.nix
@@ -42,6 +42,8 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+  
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_9;
 
   services.xserver = {
     enable = true;
